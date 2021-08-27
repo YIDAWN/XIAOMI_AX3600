b059ff01e9f3c38eaece66b31471fac593c3d584
YIDAWN <77382509+YIDAWN@users.noreply.github.com>
2021-08-28 06:15:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,7 +24,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 #添加额外非必须软件包
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 #添加smartdns
